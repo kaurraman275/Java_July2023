9fe98ac1df31af1524871bf695dafbb36d256ecb
Raman_kaur <kaurraman275@gmail.com>
2023-07-08 16:23:29 -0400

    Number is even or add

---
+++ src/main/java/July_8_2023/TernanyOperator.java
@@ -0,0 +1,15 @@
+package July_8_2023;
+import java.util.Scanner;
+
+
+public class TernanyOperator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number if even/odd?");
+        int number =scanner.nextInt();
+
+        String result = (number %2 ==0) ? "even" : "odd";
+        System.out.println(result);
+
+    }
+}

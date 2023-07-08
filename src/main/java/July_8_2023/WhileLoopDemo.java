28ba530e1579154677e7882e466c56198581c897
Raman_kaur <kaurraman275@gmail.com>
2023-07-08 17:33:10 -0400

    while loop

---
+++ src/main/java/July_8_2023/WhileLoopDemo.java
@@ -0,0 +1,12 @@
+package July_8_2023;
+
+public class WhileLoopDemo {
+    public static void main(String[] args) {
+            int counter = 1;
+            while(counter < 6) {
+                System.out.println("Hello G");
+              counter++;
+            }
+        }}
+
+
